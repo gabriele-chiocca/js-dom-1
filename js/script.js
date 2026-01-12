f1818e39f514c7dea70a8ef5747ac090064a2767
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-12 17:13:15 +0100

    Lampadina accendi

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const imageContainer = document.getElementById('lampada');
+const buttonTurn = document.getElementById('primary-button');
+
+console.log(imageContainer);
+console.log(buttonTurn);
+
+buttonTurn.addEventListener('click', handleTurnOffOrOn);
+
+/*buttonTurn.innerHTML = 'Spento'; */
+
+function handleTurnOffOrOn() {
+  const isOff = imageContainer.src.includes('white_lamp.png');
+
+  if (isOff) {
+    imageContainer.src = './img/yellow_lamp.png';
+    buttonTurn.innerHTML = 'Spento';
+  } else {
+  }
+}
